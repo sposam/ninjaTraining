18ffc141e0b7dc9959108e90128f391581e6881c
sposam <siddartha18@gmail.com>
2018-07-10 15:28:50 -0400

    adding args usecase

---
+++ chapter4/0whateverArgs.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+function whatever(a,b,c) {
+    assert(a === 1, "the value of a is 1");
+    assert(b === 2, "the value of b is 2");
+    assert(c === 3, "the value of c is 3");
+
+    assert(arguments.length === 5, "The number of arguments passed is 5");
+
+    console.log(arguments);
+    console.log(arguments[3]);
+    console.log(arguments[4]);
+
+}
+
+whatever(1,2,3,4,'Lauren!');
